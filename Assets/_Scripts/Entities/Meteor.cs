194d1f9615344e91d1c4f6406197a6b3f99a1dc7
Pedro Xudre <pedroxudre@gmail.com>
2018-07-19 16:28:31 -0300

    Meteor variations at startup

--- Assets/_Scripts/Entities/Meteor.cs
+++ Assets/_Scripts/Entities/Meteor.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
+using Random = System.Random;
 
 namespace Asteroids
 {
   public class Meteor : Destructible
   {
+
+    private static Random _random;
+
+    [SerializeField]
+    private Sprite[] _variations;
+
+    private SpriteRenderer _renderer;
+
+    private void Start()
+    {
+      _renderer = GetComponent<SpriteRenderer>();
+
+      if (_variations == null)
+        return;
+
+      if (_random == null)
+        _random = new Random();
+
+      _renderer.sprite = _variations[_random.Next(0, _variations.Length)];
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
